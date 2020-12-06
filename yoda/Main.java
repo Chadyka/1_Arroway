af933c5b89043fb4fb59a6841ef2ce98145835e2
chadyka <adamjakab13@gmail.com>
2020-12-06 21:51:40 +0100

    Repo init, started work

--- yoda/Main.java
+++ yoda/Main.java
@@ -4,12 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
         
-        /*
-            Amennyiben null az érték amivel szemben tesztelünk
-            ebben az esetben helytelenül exceptiont kapunk
-            ahelyett, hogy szimplán hamist adna vissza.
-        */
         String myString = null;
+
+        /*
+        *   Az első esetben nem használjuk a yoda
+        *   conditions-t, az equals ezesetben hamis lesz
+        *   és az else ág lefut
+        */
+
+        if ("yoda".equals(myString)) {
+            System.out.println("exception is thrown is it not?");
+        } else {
+            System.out.println("exception it is not");
+        }
+
+        /*
+        *    Amennyiben null az érték amivel szemben tesztelünk
+        *    ebben az esetben exceptiont kapunk
+        *    ahelyett, hogy szimplán hamist adna vissza
+        *    emiatt megúszhatunk egy NullPtr exception-t később
+        */
         if (myString.equals("yoda")) {
             System.out.println("exception is thrown is it not?");
         } else {
